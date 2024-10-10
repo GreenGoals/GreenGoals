6d8c3be313d72e8ae99845ba89731b2632cff209
3la2i <alaa.ata25@gmail.com>
2024-10-10 10:20:08 +0300

    alaa

--- greengoals/src/app/layout.js
+++ greengoals/src/app/layout.js
@@ -1,6 +1,9 @@
 import localFont from "next/font/local";
 import "./globals.css";
 
+
+import Header from "./navbar/page";
+import Footer from "./footer/page";
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
@@ -23,7 +26,12 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+
+      <Header/>
         {children}
+<Footer/>
+
+
       </body>
     </html>
   );
